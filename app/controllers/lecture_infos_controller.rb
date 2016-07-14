58de25acfb6ebf29b4d93a1ab1e008a841b7f2a4
seunghyum <antmgu@naver.com>
2016-07-14 10:07:37 +0900

    강의평가 필터 추가

--- app/controllers/lecture_infos_controller.rb
+++ app/controllers/lecture_infos_controller.rb
@@ -2,12 +2,21 @@ class LectureInfosController < ApplicationController
   before_action :set_lecture_info, only: [:show, :edit, :update, :destroy]
 
   def index
-    if params[:category] == "강의명" || params[:category].nil?
+    if params[:category] == "강의명" || params[:category].nil? || params[:filter].nil?
       @lecture_infos = LectureInfo.search(params[:search])
-    elsif params[:category] == "교수 이름"
-      @lecture_infos = LectureInfo.professor_search(params[:professor])
+    else
+      @lecture_infos = LectureInfo.search(params[:search]).where(l_type: params[:filter])
     end
 
+    if params[:category] == "교수 이름" && params[:filter].nil?
+      @lecture_infos = LectureInfo.professor_search(params[:professor])
+    else
+      @lecture_infos = LectureInfo.professor_search(params[:professor]).where(l_type: params[:filter])
+    end
+
+    if params[:filter] == "전체"
+      @lecture_infos = LectureInfo.all
+    end
     @best_liberal = BestFive.find_by(category: "교양").lecture_infos
   end
 
