3066e4893f6cfd57ecea30d32bdfcf0a5c7af62a
seunghyum <antmgu@naver.com>
2016-07-20 02:31:09 +0900

    ignore

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                                       # required
-    :aws_access_key_id      => 'AKIAIO7NM6R36N5FSP3Q',                      # required
-    :aws_secret_access_key  => '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',  # required
+    :aws_access_key_id      => 'yy',                      # required
+    :aws_secret_access_key  => 'xx',  # required
     :region                 => 'ap-northeast-2',                            # optional, defaults to 'us-east-1'
     :endpoint               => "https://s3.ap-northeast-2.amazonaws.com"
   }
