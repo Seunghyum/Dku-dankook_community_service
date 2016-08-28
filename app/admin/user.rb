a83bfc05b4d6a486cb6f6920dec6340e5a8d9c84
seunghyum <seunghyun257@naver.com>
2016-08-29 02:37:26 +0900

    어드민페이지 오류수정 및 iba사물함 박스 수정

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register User do
 
-  permit_params :email, :password, :password_confirmation, :role, :username, :lnum, :name, :major_id
+  permit_params :email, :password, :password_confirmation, :role, :username, :lnum, :name, :major_id, :certification, :use_pause
 
   index do
       column :email
@@ -36,8 +36,12 @@ ActiveAdmin.register User do
             외부인: "외부인", 슈퍼관리자: "슈퍼관리자", 학생: "학생", 일반대표: "일반대표", 인액터스: "인액터스"
           }
           f.input :use_pause, as: :radio, collection: {
+            사용정지: true, 사용가능: false
+          }
+          f.input :certification, as: :radio, collection: {
             인증: true, 미인증: false
           }
+
       end
       f.actions
   end
