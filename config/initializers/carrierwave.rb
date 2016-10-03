f868e38cbe3d9b665b165bb5d9b5adb510fe040b
seunghyum <seunghyun257@naver.com>
2016-10-03 14:32:00 +0900

    carrierwave fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,2 +1,8 @@
 require 'carrierwave/orm/activerecord'
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+
+CarrierWave.configure do |config|
+  config.permissions = 0600
+  config.directory_permissions = 0700
+  config.root = Rails.root
+end
